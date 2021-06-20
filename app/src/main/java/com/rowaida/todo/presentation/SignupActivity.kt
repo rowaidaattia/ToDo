15accd37f9652469f7e7c98a3938bd37a1850e69
Rowaida Attia <rowaida.attia@vodafone.com>
2021-06-20 11:24:50 +0200

    updated

--- app/src/main/java/com/rowaida/todo/presentation/SignupActivity.kt
+++ app/src/main/java/com/rowaida/todo/presentation/SignupActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import com.rowaida.todo.R
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
@@ -29,6 +32,7 @@ class SignupActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG).show()
 
             //add user
+
 
             //go to notes activity
             val intent = Intent(this, NotesActivity::class.java)
